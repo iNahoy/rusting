1db277c029c90356d55edcc90b0469904b9d67e7
Nahoy <jorgeyohandev@gmail.com>
2026-01-27 16:07:21 -0300

    pequeno coding challenge do curso

---
+++ about_me/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("I've been here this whole time");
+    println!("You were asleep...");
+    println!("It's time to wake up");
+    /* começo do clipe de jumpsuit do twenty one pilots
+     * baita música. acho q fala bastante de mim nesse momento
+     */
+    print!("abada abada, salve do taz mania");
+    print!("teste de quebra de linha")
+    /* diferença entre println! e print!
+     * println! -> envolve quebra de linha
+     * print! -> não envolve quebra de linha
+     */
+}

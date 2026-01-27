31a34dfad2771775674d5dd917ea5cac533b5dee
Nahoy <jorgeyohandev@gmail.com>
2026-01-27 15:59:55 -0300

    teste de comentario 2

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     //teste de comentario
+    /*teste de comentario 2 a vingança */
     println!("Hello World!!");
     println!("Eu sou programador em Rust e Corinthiano, porra!!!");
 }

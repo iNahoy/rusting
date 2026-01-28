1c64455a54eb8412d0837be376884c35a1ac44d8
Yohan <goodbyenohay@gmail.com>
2026-01-28 15:10:54 -0300

    feat: shadowing finalizado

--- mod2-variables_and_mutability/src/shadowing.rs
+++ mod2-variables_and_mutability/src/shadowing.rs
@@ -1,1 +1,8 @@
-pub fn shadowing() {}
+pub fn executar() {
+    /*shadowing é basicamente, sombrear as variáveis com outros tipos
+    aqui temos exemplo de uma variavel que pode se transformar em outras e ainda terá o mesmo resultado
+    */
+    let grams_of_protein: &str = "100.345";
+    let grams_of_protein: f64 = 100.345;
+    let grams_of_protein: i32 = 100;
+}

5819547e3b59100550eed40b4c3b204573b16a34
Yohan <goodbyenohay@gmail.com>
2026-01-28 14:32:16 -0300

    feat: interpolação + variáveis

--- mod2-variables_and_mutability/src/main.rs
+++ mod2-variables_and_mutability/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     let apples: i32 = 50;
+    let oranges: i32 = 14 + 6;
+    let _fruits: i32 = apples + oranges;
+
+    // primeira forma de interpolação
+    println!("My garden has {} apples", apples);
+    //segunda forma de interpolação
+    println!("My garden has {apples} apples");
+
+    //composta agora
+    // primeira forma
+    println!("My garden has {} apples and {} oranges.", apples, oranges);
+    // segunda forma
+    println!("My garden has {apples} apples and {oranges} oranges.");
+    /* terceira forma, puxa a ordem da linguagem pra que funcione
+     * 0 = apples
+     * 1 = oranges
+     */
+    println!("My garden has {0} apples and {1} oranges.", apples, oranges);
+    println!("My garden has {1} apples and {0} oranges.", apples, oranges);
 }

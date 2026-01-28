4cdfcae17fb7855815c22c1b9856f0ec0a0b1f46
Yohan <goodbyenohay@gmail.com>
2026-01-28 16:52:04 -0300

    feat: type_aliases upd

--- mod2-variables_and_mutability/src/type_aliases.rs
+++ mod2-variables_and_mutability/src/type_aliases.rs
@@ -1,8 +1,18 @@
 type Meters = i32;
 /* type aliases é o mesmo que descrever um tipo especifico para formatação
 aqui utilizamos metros para ser equivalente ao interger, é útil para uma informação dita melhor*/
+#[allow(unused_variables)]
 pub fn executar() {
     println!("--- AULA: Type Aliases ---");
+
+    // pouco de uso de compiler directives
+    /*aqui tamo brincando um pouco com variaveis que não são utilizadas
+     basicamente, podemos usar isso nas linhas que na seguinte terão variáveis que não são utilizadas ou
+    colocar algo como coloquei antes da função */
+    // #[allow(unused_variables)]
+    let variavelnaousada = "only a test";
+    // #[allow(unused_variables)]
+    let variavelnaousada = "hahaha";
 
     let mile_race: Meters = 1600;
     let two_mile_race: Meters = 3200;

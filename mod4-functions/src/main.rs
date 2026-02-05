e7af282156a9dc52e39e3f2f38bc2d08adf505e1
Yohan <goodbyenohay@gmail.com>
2026-02-05 09:50:38 -0300

    feat: funções iniciais e parametros adicionados

---
+++ mod4-functions/src/main.rs
@@ -0,0 +1,18 @@
+fn open_store(neighbourhood: &str) {
+    println!("Opening my pizza store!\nIn {neighbourhood}!")
+}
+fn closing_store() {
+    println!("Closing my pizza store!")
+}
+
+fn main() {
+    open_store("Brooklyn");
+    bake_pizza(8, "pepperoni");
+    open_store("Amaraji");
+    bake_pizza(21, "sinistras");
+    open_store("Itaquera");
+    closing_store();
+}
+fn bake_pizza(number: u8, topping: &str) {
+    println!("Baking {} {} pizzas!", number, topping)
+}

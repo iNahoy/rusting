1c671f278e2ba97378e73570b76aef819e698d4b
Yohan <goodbyenohay@gmail.com>
2026-02-05 10:08:31 -0300

    feat: end mod4

--- mod4-functions/src/main.rs
+++ mod4-functions/src/main.rs
@@ -14,7 +14,10 @@ fn main() {
     closing_store();
 
     let result = square(2);
-    println!("{}", result)
+    println!("{}", result);
+
+    let resultado = calculation(2);
+    println!("{}", resultado);
 }
 fn bake_pizza(number: u8, topping: &str) {
     println!("Baking {} {} pizzas!", number, topping)
@@ -22,4 +25,10 @@ fn bake_pizza(number: u8, topping: &str) {
 
 fn square(num: u8) -> u8 {
     return num * num;
+    // num * num tambem retornaria 4 neste caso, sem ; nem return
+}
+fn calculation(n: u8) -> u8 {
+    let multiplier = 3;
+    let value = 5 + 4;
+    (value + n) * multiplier
 }

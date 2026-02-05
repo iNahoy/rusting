6d1404359a4a48a4e3fc0ba1f6ce7a566ce40da3
Yohan <goodbyenohay@gmail.com>
2026-02-05 09:55:25 -0300

    feat: return add

--- mod4-functions/src/main.rs
+++ mod4-functions/src/main.rs
@@ -12,7 +12,14 @@ fn main() {
     bake_pizza(21, "sinistras");
     open_store("Itaquera");
     closing_store();
+
+    let result = square(2);
+    println!("{}", result)
 }
 fn bake_pizza(number: u8, topping: &str) {
     println!("Baking {} {} pizzas!", number, topping)
+}
+
+fn square(num: u8) -> u8 {
+    return num * num;
 }
